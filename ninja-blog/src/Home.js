19c7bb3920babbe4d8d8077837790befffc95fca
ephraim <ephraims.818@gmail.com>
2025-07-29 20:13:37 +0600

    REACT--NET-NINJA - 04. Click Events

--- ninja-blog/src/Home.js
+++ ninja-blog/src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+    const handleClick = function (e) {
+        console.log("Hello ninjas", e.target);
+    };
+    const handleClickAgain = function (name) {
+        console.log("Hello " + name);
+    };
     return (
         <div className="home">
             <h1>Home</h1>
+            <button onClick={handleClick}>Click me</button>
+            <button
+                onClick={() => {
+                    handleClickAgain("Mario");
+                }}
+            >
+                Click me again
+            </button>
         </div>
     );
 };
