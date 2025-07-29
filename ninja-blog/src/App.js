85ea76ffe36c0d4684c975529d41f363890086a2
ephraim <ephraims.818@gmail.com>
2025-07-29 18:25:18 +0600

    REACT--NET-NINJA - 02. Multiple Components

--- ninja-blog/src/App.js
+++ ninja-blog/src/App.js
@@ -1,19 +1,15 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-    const title = "Welcome to the blog";
-    const likes = 50;
-    const link = "https://www.google.com";
-
     return (
         <div className="App">
-            <h1>{title}</h1>
-            <p>Liked ❤{likes} times</p>
-            <p>{5 + 5}</p>
-            <p>{"Hello ninjas"}</p>
-            <p>{[1, 2, 3, 4, 5]}</p>
-            <p>{Math.floor(Math.random() * 20) + 1}</p>
-            <a href={link}>Google</a>
+            <Navbar />
+            <div className="content">
+                <h1>App Component</h1>
+                <Home />
+            </div>
         </div>
     );
 }
