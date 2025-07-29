64541df41d171c66ca82386d1773f806c5fb2c2c
ephraim <ephraims.818@gmail.com>
2025-07-29 06:05:29 +0600

    REACT--NET-NINJA - 01. Dynamic Values in Templates

--- ninja-blog/src/App.js
+++ ninja-blog/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    const title = "Welcome to the blog";
+    const likes = 50;
+    const link = "https://www.google.com";
+
+    return (
+        <div className="App">
+            <h1>{title}</h1>
+            <p>Liked ❤{likes} times</p>
+            <p>{5 + 5}</p>
+            <p>{"Hello ninjas"}</p>
+            <p>{[1, 2, 3, 4, 5]}</p>
+            <p>{Math.floor(Math.random() * 20) + 1}</p>
+            <a href={link}>Google</a>
+        </div>
+    );
 }
 
 export default App;
