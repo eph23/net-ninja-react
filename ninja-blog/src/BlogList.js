4faf60ddeac49d04e87c3d230ea98e5a9dca5f71
ephraim <ephraims.818@gmail.com>
2025-07-30 19:54:50 +0600

    REACT--NET-NINJA - 09. Functions as Props

--- ninja-blog/src/BlogList.js
+++ ninja-blog/src/BlogList.js
@@ -1,13 +1,14 @@
-const BlogList = ({ blogs, title }) => {
-    //     const blogs = props.blogs;
-    //     const title = props.title;
+const BlogList = ({ blogs, title, handleDelete }) => {
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>
+                        Delete blog
+                    </button>
                 </div>
             ))}
         </div>
