aa277b1bb7ca770d846c755bcd5edaa7bf3ac95c
ephraim <ephraims.818@gmail.com>
2025-08-05 03:12:43 +0600

    REACT--NET-NINJA - 23. Controlled Inputs

--- ninja-blog/src/Create.js
+++ ninja-blog/src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Mario");
+
     return (
         <div className="create">
-            <h1>Add a new blog</h1>
+            <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                />
+                <label>Blog Body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(event) => setBody(event.target.value)}
+                ></textarea>
+                <label>Blog Author</label>
+                <select
+                    value={author}
+                    onChange={(event) => setAuthor(event.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 };
