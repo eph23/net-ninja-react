93d67f6481c32eb097f1a814d670dd01b677eeeb
ephraim <ephraims.818@gmail.com>
2025-08-07 03:39:26 +0600

    REACT--NET-NINJA - 27. Deleting Blogs

--- ninja-blog/src/BlogDetails.js
+++ ninja-blog/src/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
@@ -8,7 +9,15 @@ const BlogDetails = () => {
         error,
         isPending,
     } = useFetch("http://localhost:8000/blogs/" + id);
+    const history = useHistory();
 
+    const handleDelete = () => {
+        fetch("http://localhost:8000/blogs/" + blog.id, {
+            method: "DELETE",
+        }).then(() => {
+            history.push("/");
+        });
+    };
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -18,6 +27,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by: {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete Blog</button>
                 </article>
             )}
         </div>
